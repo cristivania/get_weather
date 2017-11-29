a4d340cbd9c5a0470301c7e0556ce0079d08731f
Cristina Rosario <crosariobatres@yahoo.com>
2017-11-28 19:24:04 -0500

    Controllers

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,18 @@
 class SessionsController < ApplicationController
-  def new
-  end
-
   def create
+    @user = User.find_by(email: params[:email])
+    if @user and @user.authenticate(params[:password])
+      #logged in
+      session[:user_id] = @user.id
+      redirect_to dashboard_path, notice: 'You have succesfully logged in.'
+    else
+      flash[:notice]= "Something was wrong please try again."
+      render :new
+    end
   end
 
   def destroy
+    reset_session
+    redirect_to login_path, notice: 'You have succesfully logout.'
   end
 end
