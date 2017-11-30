3651137627ec2302f0fc5d68e6eda79dc0ff0d63
Cristina Rosario <crosariobatres@yahoo.com>
2017-11-30 18:51:23 -0500

    Http Party get_weather

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -12,12 +12,17 @@ class PlacesController < ApplicationController
 
   def create
     @place = current_user.places.build(place_params)
-    if @place.save
-      redirect_to user_place_path , notice: 'City saved'
-    else
-      render :new
-    end
+
+    respond_to do |format|
+  if @place.save
+    format.html { redirect_to user_place_path(current_user), notice: 'City was successfully created.' }
+    format.json { render :show, status: :created, location: @place }
+  else
+    format.html { render :new }
+    format.json { render json: @place.errors, status: :unprocessable_entity }
   end
+end
+end
 
   def show
     @place = current_user.places.find(params[:id])
--- app/helpers/places_helper.rb
+++ app/helpers/places_helper.rb
@@ -1,2 +1,6 @@
 module PlacesHelper
+  def get_weather(place)
+   response = HTTParty.get("http://api.openweathermap.org/data/2.5/weather?q=#{city_name}&units=imperial&appid=#{ENV['OPENWEATHER_KEY']}")
+   return response.parsed_response
+ end
 end
